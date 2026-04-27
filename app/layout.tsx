4c7011493e9419b6100aa3ab22609c040c292660
Yatharth2609 <yatharth.mishra2002@gmail.com>
2026-04-27 13:22:19 +0530

    vercel-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
+import { Analytics } from '@vercel/analytics/next';
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
@@ -51,6 +52,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
